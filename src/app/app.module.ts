1227700d21be11b39f278dea28944f84290513c4
Your Name <you@example.com>
2022-09-16 11:28:11 -0500

    16/09/2022

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { IntegrantesComponent } from './integrantes/integrantes.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { AlbumesComponent } from './albumes/albumes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent,
+    IntegrantesComponent,
+    HistoriaComponent,
+    AlbumesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
