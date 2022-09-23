1587ca0207c46810111a8b81b3553c461cda322e
Your Name <you@example.com>
2022-09-23 11:14:31 -0500

    23/09/2022

--- src/app/albumes/albumes.component.ts
+++ src/app/albumes/albumes.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TopService } from '../services/top.service';
 
 @Component({
   selector: 'app-albumes',
   templateUrl: './albumes.component.html',
   styleUrls: ['./albumes.component.css']
 })
-export class AlbumesComponent implements OnInit {
+export class AlbumesComponent  {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  public respuestaServicio:any[]=[]
+
+  constructor(public peticion:TopService) {
+    //como utilizar los datos que llegan de un api
+
+    this.peticion.buscar()
+    .subscribe(respuesta=>{
+      this.respuestaServicio=respuesta.datos
+      console.log(this.respuestaServicio)
+    })
+   }
+
+
 
 }
